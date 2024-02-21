995a859ab985f7deff91be0b0bae37e1c66d62c3
Markus Ankenbrand <markus.ankenbrand@uni-wuerzburg.de>
2024-02-21 14:52:28 +0100

    Add test for current_tetromino function

---
+++ test_current_tetromino.py
@@ -0,0 +1,25 @@
+import unittest
+from pyboy import PyBoy
+from fake_ct import current_tetromino
+
+class TestCurrentTetromino(unittest.TestCase):
+
+    def setUp(self):
+        self.pyboy = PyBoy("Tetris.gb", game_wrapper=True)
+        self.pyboy.set_emulation_speed(0)
+        self.tetris = self.pyboy.game_wrapper()
+        self.tetris.start_game(timer_div=0x00)
+    
+    def test_tetromino_0(self):
+        self.tetris.reset_game(timer_div=0)
+        ct = current_tetromino(self.tetris)
+        self.assertEqual(ct, "J")
+
+    def test_tetrominos_1_to_9(self):
+        expectation = "IOZSTOJIO"
+        for i,t in enumerate(expectation):
+            self.tetris.reset_game(timer_div=i+1)
+            self.assertEqual(current_tetromino(self.tetris), t)
+        
+if __name__ == '__main__':
+    unittest.main()
