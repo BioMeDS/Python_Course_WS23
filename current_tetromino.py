e8e9bb5f367f2985fe4b4ce0927245457f41c599
Markus Ankenbrand <markus.ankenbrand@uni-wuerzburg.de>
2024-02-22 14:55:40 +0100

    Add get_shape to current_tetromino

--- current_tetromino.py
+++ current_tetromino.py
@@ -32,3 +32,17 @@ def current_tetromino(tetris):
     if matching.sum() < 1:
         return None
     return shape_names[np.argmax(matching)]
+
+def get_shape(shape_name, crop=False):
+    ind = shape_names.index(shape_name)
+    if ind < 0:
+        return None
+    shape = shapes[ind]
+    if crop:
+        if shape[:,3].sum() == 0:
+            shape = shape[:,:3]
+        if shape[:,0].sum() == 0:
+            shape = shape[:,1:]
+        if shape[1,:].sum() == 0:
+            shape = shape[:1,:]
+    return shape
