3712fdcf36ef34e0f291214b2b6bdee35f687dc9
Markus Ankenbrand <markus@ankenbrand.me>
2024-07-12 13:57:16 +0200

    Don't close the emulator window after fail

--- 10-putting-it-together.py
+++ 10-putting-it-together.py
@@ -1,31 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[3]:
-
-
 from pyboy import PyBoy, WindowEvent
 import numpy as np
-
-
-# In[4]:
-
-
 import current_tetromino as ct
 from possibilities import get_possibilities
 import scoring_functions as sf
 import moving_instructions as mi
 
-
-# In[5]:
-
-
 def game_over(tetris):
     return tetris.game_area().base[-2,-2] == 39
-
-
-# In[6]:
-
 
 def play_until_next_tetro(pyboy, tetris):
     while not ct.current_tetromino(tetris):
@@ -33,17 +17,9 @@ def play_until_next_tetro(pyboy, tetris):
         if game_over(tetris):
             break
 
-
-# In[7]:
-
-
 def score(possibility):
     field = possibility[0]
     return 4*sf.recognize_holes(field)+max(sf.get_heights(field))-10*sf.lines_cleared_at_same_time(field)
-
-
-# In[8]:
-
 
 def get_starting_col(tetris, rotations):
     current = ct.current_tetromino(tetris)
@@ -56,24 +32,11 @@ def get_starting_col(tetris, rotations):
         start_col = 4
     return start_col
 
-
-# In[ ]:
-
-
-pyboy = PyBoy("Tetris.gb", game_wrapper=True)
+pyboy = PyBoy("Tetris.gb", game_wrapper=True)#, sound=True)
 pyboy.set_emulation_speed(10)
 tetris = pyboy.game_wrapper()
 tetris.start_game()
-
-
-# In[51]:
-
-
 tetris.reset_game()#timer_div=0)
-
-
-# In[52]:
-
 
 while not game_over(tetris):
     possibilities = get_possibilities(tetris)
@@ -83,9 +46,9 @@ while not game_over(tetris):
     [pyboy.tick() for _ in range(30)]
     play_until_next_tetro(pyboy,tetris)
 
+pyboy.set_emulation_speed(1)
+print(tetris.score)
 
-# In[ ]:
-
-
-
-
+# Keep the game running forever
+while True:
+    pyboy.tick()
